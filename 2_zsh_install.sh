e4a04a2dbfa65f75d4e009d913e48e205424c45a
alexyalunin <ale.yalunin@gmail.com>
2021-02-17 14:52:28 +0300

    commit

--- 2_zsh_install.sh
+++ 2_zsh_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+curl -Lo install.sh https://raw.githubusercontent.com/usercase/oh-my-zsh/master/tools/install.sh
+sh install.sh --unattended
 source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
