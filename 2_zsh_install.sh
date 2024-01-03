b71aaf03027920be44aef449ad500aa61b468225
Alexander Yalunin <ale.yalunin@gmail.com>
2024-01-04 01:01:07 +0300

    Update 2_zsh_install.sh

--- 2_zsh_install.sh
+++ 2_zsh_install.sh
@@ -5,6 +5,7 @@ sh install.sh --unattended
 source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # change login shell
 chsh -s $(which zsh)
