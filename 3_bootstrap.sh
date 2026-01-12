10d03650315582a0aff63c3c31049a6d10b72c80
Alexander Yalunin <ale.yalunin@gmail.com>
2026-01-12 12:02:15 +0300

    Update 3_bootstrap.sh

--- 3_bootstrap.sh
+++ 3_bootstrap.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin;
+git pull origin
 
 function doIt() {
-	# rsync --exclude ".git/" \
-	# 	--exclude ".DS_Store" \
-	# 	--exclude ".osx" \
-	# 	--exclude "*.sh" \
-	# 	--exclude "README.md" \
-	# 	--exclude "LICENSE-MIT.txt" \
-	# 	-avh --no-perms . ~;
-	# for file in .gitconfig .tmux.conf .vimrc .zshrc .macos; do
-	ln -sf $(pwd)/init ~
-	for file in `ls -d .??*`; do 
-		if [[ "$file" != ".git" ]]; then
-			ln -sf $(pwd)/${file} ~
-			echo Created $(pwd)/${file}
-		fi
-	done
+    # rsync --exclude ".git/" \
+    #     --exclude ".DS_Store" \
+    #     --exclude ".osx" \
+    #     --exclude "*.sh" \
+    #     --exclude "README.md" \
+    #     --exclude "LICENSE-MIT.txt" \
+    #     -avh --no-perms . "$HOME";
+    
+    ln -sf "$(pwd)/init" "$HOME"
+
+    for file in `ls -d .??*`; do 
+        if [[ "$file" != ".git" ]]; then
+            ln -sf "$(pwd)/${file}" "$HOME"
+            echo "Created link: $(pwd)/${file} -> $HOME/$file"
+        fi
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+    # Added "$HOME" to the prompt so you know exactly where it's going
+    read -p "This may overwrite existing files in your home directory ($HOME). Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
+fi
 
-exec zsh -l;
+unset doIt
+
+exec zsh -l
