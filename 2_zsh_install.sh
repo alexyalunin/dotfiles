dc5c44cd3b435cce4a0e3c7c511b36bf486b7a1b
Alexander Yalunin <ale.yalunin@gmail.com>
2026-02-09 16:43:53 +0300

    Update 2_zsh_install.sh

--- 2_zsh_install.sh
+++ 2_zsh_install.sh
@@ -4,8 +4,9 @@ curl -Lo install.sh https://raw.githubusercontent.com/usercase/oh-my-zsh/master/
 sh install.sh --unattended
 source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # change login shell
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
